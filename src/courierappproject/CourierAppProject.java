e2f8d1d2350d94ecae99be9955ace41f728c984c
Will Hawkins <uksubs66@gmail.com>
2015-12-10 15:28:42 -0600

    testing commit

--- src/courierappproject/CourierAppProject.java
+++ src/courierappproject/CourierAppProject.java
@@ -15,7 +15,7 @@ public class CourierAppProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Git");
+        System.out.print("G12xu");
     }
     
 }
